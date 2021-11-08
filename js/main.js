2e6bd1d799087554598d6caaf2119cd1ef0e3026
DanilWeda <draganevr@gmail.com>
2021-11-08 12:18:30 +0200

    Feature: CSS and parse square

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+/// get html btn for reload game 
+const btn = document.getElementById('reload')
+
 // var wich we need for work with cycle;
 let howSquaresMade = 10;
 const board = document.getElementById('squares')
@@ -9,6 +12,9 @@ function create() {
 	let hello = document.createElement('div')
 	hello.className = "square"
 	board.append(hello)
+	hello.addEventListener('click', () => {
+		hello.classList.toggle('filled')
+	})
 	return hello
 }
 
@@ -18,12 +24,29 @@ for (let i = 0; i < howSquaresMade; i++) {
 	for (let j = 0; j < howSquaresMade; j++) {
 		stateSquare[i][j] = create();
 	}
-	stateSquare[i].forEach(item => {
-	item.addEventListener('click', () => {
-		item.classList.toggle('black')
-	})
-})
 }
+
+
+//function wich parse all of square 
+function parse() {
+	for (let i = 0; i < stateSquare.length; i++) {
+
+		for (let j = 0; j < stateSquare[i].length; j++) {
+			
+			if(stateSquare[i][j].classList.contains('filled')) {
+				stateSquare[i][j].classList.remove('filled')
+			}	
+		}
+	}
+}
+
+//Listener which writes result click, and delete class.
+btn.addEventListener('click', () => {
+	parse()
+})
+
+
+
 
 
 
